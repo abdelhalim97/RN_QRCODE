ae48c4e08676e632a7303b090f992bb5edd72258
unknown <abdelhalimbenoun@gmail.com>
2022-11-13 16:07:47 +0100

    gradiesnt pages setup

--- App.tsx
+++ App.tsx
@@ -1,41 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { type PropsWithChildren } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login } from './src/components';
 const App = () => {
-
-
+  const Stack = createNativeStackNavigator();
   return (
     <SafeAreaView >
       <StatusBar />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic">
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} options={{ title: 'Welcome', headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
       </ScrollView>
     </SafeAreaView>
   );
